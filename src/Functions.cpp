e3bfb25ace7b2bd81a374ef70abd9e6417cb2568
Charly Lersteau <charly.lersteau@gmx.fr>
2012-12-25 15:14:55 +0100

    Extend to p-objective and some C++ clean up

--- src/Functions.cpp
+++ src/Functions.cpp
@@ -20,6 +20,7 @@
 
 #include "Functions.hpp"
 #include "Argument.hpp"
+#include <limits>
 
 long int createBox(std::vector<Box*> &vectorBox, Data &data)
 {
@@ -27,48 +28,48 @@ long int createBox(std::vector<Box*> &vectorBox, Data &data)
 		
 	//Add the first boxes with only one facility opened
 	Box *box0 = new Box(data);
-	addChildren(box0,vectorBox);
+	addChildren(box0, vectorBox);
 	delete box0;
 	
-	for(unsigned int it = 0; it < vectorBox.size();)
+	for (unsigned int it = 0; it < vectorBox.size();)
 	{
-		nbBoxComputed++;
+		++nbBoxComputed;
 		//If the box is dominated by its origin by all the existing boxes
-		if( isDominatedByItsOrigin(vectorBox,vectorBox[it]) )
+		if ( isDominatedByItsOrigin(vectorBox, vectorBox[it]) )
 		{
 			delete vectorBox[it];
-			vectorBox.erase(vectorBox.begin() += it);
+			vectorBox.erase(vectorBox.begin() + it);
 		}
 		else
 		{
 			//Compute all potential children of this box as candidates boxes
-			addChildren(vectorBox[it],vectorBox);
+			addChildren(vectorBox[it], vectorBox);
 			//Compute bounds of this box
 			vectorBox[it]->computeBox();
 			//If this box is dominated by all the boxes (existing + its children) regarding to its bounds
-			if(isDominatedByItsBox(vectorBox,vectorBox[it]))
+			if (isDominatedByItsBox(vectorBox, vectorBox[it]))
 			{
 				delete vectorBox[it];
-				vectorBox.erase(vectorBox.begin() += it);
+				vectorBox.erase(vectorBox.begin() + it);
 			}
 			else
 			{
 				//If one of all others boxes are dominated by this box, we delete it
-				for(unsigned int it2 = 0; it2 != it;)
+				for (unsigned int it2 = 0; it2 != it;)
 				{
-					if(isDominatedBetweenTwoBoxes(vectorBox[it2],vectorBox[it]))
+					if (isDominatedBetweenTwoBoxes(vectorBox[it2], vectorBox[it]))
 					{
 						delete vectorBox[it2];
-						vectorBox.erase(vectorBox.begin() += it2);
+						vectorBox.erase(vectorBox.begin() + it2);
 						//We delete an element before (the list shift one step forward), so we shift one step backward
-						it--;
+						--it;
 					}
 					else
 					{
-						it2++;
+						++it2;
 					}	
 				}
-				it++;
+				++it;
 			}
 		}
 	}
@@ -90,9 +91,9 @@ void addChildren(Box *boxMother, std::vector<Box*> &vBox)
     std::vector<bool> facilityOpen(data.getnbFacility());
     int indexLastOpened = -1;
     
-    for(unsigned int i = 0 ; i < data.getnbFacility() ; i++)
+    for (unsigned int i = 0 ; i < data.getnbFacility() ; ++i)
     {
-        if(boxMother->isOpened(i))
+        if (boxMother->isOpened(i))
         {
             indexLastOpened = i;
         }
@@ -100,10 +101,10 @@ void addChildren(Box *boxMother, std::vector<Box*> &vBox)
     }
     
     //For each digit 0 of facilities not yet affected, we change it in 1 to create the corresponding a new combination
-    for(unsigned int i = indexLastOpened + 1 ; i < data.getnbFacility() ; i++)
+    for (unsigned int i = indexLastOpened + 1 ; i < data.getnbFacility() ; ++i)
     {
         facilityOpen[i] = true;
-        Box *tmp = new Box(data,facilityOpen);
+        Box *tmp = new Box(data, facilityOpen);
         vBox.push_back(tmp);		
         facilityOpen[i] = false;
     }
@@ -112,34 +113,35 @@ void addChildren(Box *boxMother, std::vector<Box*> &vBox)
 void filter(std::vector<Box*> &vectorBox, long int &nbToCompute, long int &nbWithNeighbor)
 {
 	//Initializing booleans
-	for(unsigned int it = 0; it < vectorBox.size();it++)
+	for (unsigned int it = 0; it < vectorBox.size(); ++it)
 	{
 		vectorBox[it]->setHasNeighbor(false);
 	}
 	//Filtering and update of booleans
-	for(unsigned int it = 0; it < vectorBox.size();)
+	for (unsigned int it = 0; it < vectorBox.size();)
 	{	
-		for(unsigned int it2 = it + 1 ; it2 < vectorBox.size();)
+		for (unsigned int it2 = it + 1 ; it2 < vectorBox.size();)
 		{
-			if( isDominatedBetweenTwoBoxes(vectorBox[it2] , vectorBox[it]) )
+			if ( isDominatedBetweenTwoBoxes(vectorBox[it2] , vectorBox[it]) )
 			{
 				delete vectorBox[it2];
-				vectorBox.erase(vectorBox.begin() += it2);
+				vectorBox.erase(vectorBox.begin() + it2);
 			}
 			else
 			{
-				if( isDominatedBetweenTwoBoxes(vectorBox[it] , vectorBox[it2]) )
+				if ( isDominatedBetweenTwoBoxes(vectorBox[it] , vectorBox[it2]) )
 				{
 					delete vectorBox[it];
-					vectorBox.erase(vectorBox.begin()+=it);
+					vectorBox.erase(vectorBox.begin() + it);
 					it2 = it + 1; //Initialize it2 at the begin of the vector
 				}
 				else
 				{
-					if( (vectorBox[it]->getHasNeighbor() == false) || (vectorBox[it2]->getHasNeighbor() == false) )
+					if ( (vectorBox[it]->getHasNeighbor() == false) || (vectorBox[it2]->getHasNeighbor() == false) )
 					{
-						if((vectorBox[it]->getMinZ1() < vectorBox[it2]->getMaxZ1() && vectorBox[it]->getMinZ2() < vectorBox[it2]->getMaxZ2()) ||
-                           (vectorBox[it2]->getMinZ1() < vectorBox[it]->getMaxZ1() && vectorBox[it2]->getMinZ2() < vectorBox[it]->getMaxZ2())  )
+						// TODO: extend to p-objective
+						if((vectorBox[it]->getMinZ(0) < vectorBox[it2]->getMaxZ(0) && vectorBox[it]->getMinZ(1) < vectorBox[it2]->getMaxZ(1)) ||
+                           (vectorBox[it2]->getMinZ(0) < vectorBox[it]->getMaxZ(0) && vectorBox[it2]->getMinZ(1) < vectorBox[it]->getMaxZ(1))  )
 						{
 							vectorBox[it]->setHasNeighbor(true);
 							vectorBox[it2]->setHasNeighbor(true);
@@ -147,25 +149,25 @@ void filter(std::vector<Box*> &vectorBox, long int &nbToCompute, long int &nbWit
 					}
 				}
 				//it2 wasn't deleted
-				it2++;
+				++it2;
 			}
 		}
 		//it wasn't deleted
-		it++;
+		++it;
 	}
 	
 	//Compute counters
 	nbToCompute = 0;
 	nbWithNeighbor = 0;
-	for(unsigned int it = 0; it < vectorBox.size();it++)
+	for (unsigned int it = 0; it < vectorBox.size(); ++it)
 	{
-		if( vectorBox[it]->getHasMoreStepWS() )
+		if ( vectorBox[it]->getHasMoreStepWS() )
 		{
-			nbToCompute++;
+			++nbToCompute;
 		}
-		if(vectorBox[it]->getHasNeighbor() )
+		if (vectorBox[it]->getHasNeighbor() )
 		{
-			nbWithNeighbor++;
+			++nbWithNeighbor;
 		}
 	}
 }
@@ -176,9 +178,9 @@ void boxFiltering(std::vector<Box*> &vectorBox, Data &data, long int &nbToComput
 	filter(vectorBox, nbToCompute, nbWithNeighbor);
 	
 	//Parameter turns to 3
-	while(nbWithNeighbor != 0 && i <= 3)
+	while (nbWithNeighbor != 0 && i <= 3)
 	{
-		i++;
+		++i;
 		weightedSumOneStep(vectorBox, data);
 		filter(vectorBox, nbToCompute, nbWithNeighbor);
 	}
@@ -195,7 +197,7 @@ void recomposition(std::vector<Box*> &vectorBox, std::vector<Box*> &vectorBoxFin
 	std::map<std::string,int> mapBox;
 	std::map<std::string,int>::iterator itM;
 	//Add boxes in the map
-	for(unsigned int it = 0; it < vectorBox.size(); it++)
+	for(unsigned int it = 0; it < vectorBox.size(); ++it)
 	{
 		itM = mapBox.find(vectorBox[it]->getId());
 		if(itM == mapBox.end())
@@ -208,11 +210,11 @@ void recomposition(std::vector<Box*> &vectorBox, std::vector<Box*> &vectorBoxFin
 		}
 	}
 	//Creating boxes with relation to the map
-	for(itM = mapBox.begin(); itM != mapBox.end(); itM++)
+	for(itM = mapBox.begin(); itM != mapBox.end(); ++itM)
 	{
 		Box *box0 = new Box(data);
 		std::string tempString = itM->first;
-		for(unsigned int i = 0; i < tempString.size(); i++)
+		for(unsigned int i = 0; i < tempString.size(); ++i)
 		{
 			if(tempString[i] == '1')
 			{
@@ -221,34 +223,30 @@ void recomposition(std::vector<Box*> &vectorBox, std::vector<Box*> &vectorBoxFin
 		}
 		box0->setId(tempString);
 		box0->computeBox();
-		box0->setMinZ1(DBL_MAX);
-		box0->setMinZ2(DBL_MAX);
-		box0->setMaxZ1(0);
-		box0->setMaxZ2(0);
+		for (int k = 0; k < box0->getNbObjective(); ++k)
+		{
+			box0->setMinZ(k, std::numeric_limits<double>::infinity());
+			box0->setMaxZ(k, 0);
+		}
 		vectorBoxFinal.push_back(box0);
 	}
 	//Updating bounds
-	for(unsigned int it = 0; it < vectorBox.size();it++)
+	for (unsigned int it = 0; it < vectorBox.size(); ++it)
 	{
-		for(unsigned int it2 = 0; it2 < vectorBoxFinal.size();it2++)
+		for (unsigned int it2 = 0; it2 < vectorBoxFinal.size(); ++it2)
 		{
-			if(vectorBoxFinal[it2]->getId() == vectorBox[it]->getId())
+			if (vectorBoxFinal[it2]->getId() == vectorBox[it]->getId())
 			{
-				if( vectorBox[it]->getMinZ1() < vectorBoxFinal[it2]->getMinZ1() )
+				for (int k = 0; k < vectorBox[it]->getNbObjective(); ++k)
 				{
-					vectorBoxFinal[it2]->setMinZ1( vectorBox[it]->getMinZ1() );
-				}
-				if( vectorBox[it]->getMinZ2() < vectorBoxFinal[it2]->getMinZ2() )
-				{
-					vectorBoxFinal[it2]->setMinZ2(vectorBox[it]->getMinZ2());
-				}
-				if( vectorBox[it]->getMaxZ1() > vectorBoxFinal[it2]->getMaxZ1() )
-				{
-					vectorBoxFinal[it2]->setMaxZ1(vectorBox[it]->getMaxZ1());
-				}
-				if( vectorBox[it]->getMaxZ2() > vectorBoxFinal[it2]->getMaxZ2() )
-				{
-					vectorBoxFinal[it2]->setMaxZ2(vectorBox[it]->getMaxZ2());
+					if ( vectorBox[it]->getMinZ(k) < vectorBoxFinal[it2]->getMinZ(k) )
+					{
+						vectorBoxFinal[it2]->setMinZ(k, vectorBox[it]->getMinZ(k));
+					}
+					if ( vectorBox[it]->getMaxZ(k) > vectorBoxFinal[it2]->getMaxZ(k) )
+					{
+						vectorBoxFinal[it2]->setMaxZ(k, vectorBox[it]->getMaxZ(k));
+					}
 				}
 			}
 		}
@@ -266,46 +264,46 @@ void recomposition(std::vector<Box*> &vectorBox, std::vector<Box*> &vectorBoxFin
 void weightedSumOneStep(std::vector<Box*> &vectorBox, Data &data)
 {
 	unsigned int size = vectorBox.size();
-	for(unsigned int it = 0; it < size; it++)
+	for (unsigned int it = 0; it < size; ++it)
 	{
-		if( vectorBox[it]->getHasNeighbor() && vectorBox[it]->getHasMoreStepWS() )
+		if ( vectorBox[it]->getHasNeighbor() && vectorBox[it]->getHasMoreStepWS() )
 		{
-			double alpha =	( vectorBox[it]->getMaxZ1() - vectorBox[it]->getMinZ1() ) / 
-            ( vectorBox[it]->getMaxZ2() - vectorBox[it]->getMinZ2() ) ;
+			double alpha =	( vectorBox[it]->getMaxZ(0) - vectorBox[it]->getMinZ(0) ) / 
+			                ( vectorBox[it]->getMaxZ(1) - vectorBox[it]->getMinZ(1) ) ;
+
+			double z_obj1 = vectorBox[it]->getOriginZ(0);
+			double z_obj2 = vectorBox[it]->getOriginZ(1);
             
-			double z_obj1 = vectorBox[it]->getOriginZ1();
-			double z_obj2 = vectorBox[it]->getOriginZ2();
-            
-			for(unsigned int i = 0; i < data.getnbCustomer(); ++i)
+			for (unsigned int i = 0; i < data.getnbCustomer(); ++i)
 			{
-				if(! (vectorBox[it]->isAssigned(i)))
+				if (! (vectorBox[it]->isAssigned(i)))
 				{
 					int ind = -1;
-					double val = DBL_MAX;
-					for(unsigned int j = 0; j < data.getnbFacility(); ++j)
+					double val = std::numeric_limits<double>::infinity();
+					for (unsigned int j = 0; j < data.getnbFacility(); ++j)
 					{
-						if( vectorBox[it]->isOpened(j) )
+						if ( vectorBox[it]->isOpened(j) )
 						{
-							if(val > data.getAllocationObj1Cost(i,j) + alpha * data.getAllocationObj2Cost(i,j))
+							if (val > data.getAllocationObjCost(0, i, j) + alpha * data.getAllocationObjCost(1, i, j))
 							{
 								ind = j;
-								val = data.getAllocationObj1Cost(i,j) + alpha * data.getAllocationObj2Cost(i,j);
+								val = data.getAllocationObjCost(0, i, j) + alpha * data.getAllocationObjCost(1, i, j);
 							}
 						}
 					}
-					z_obj1 += data.getAllocationObj1Cost(i,ind);
-					z_obj2 += data.getAllocationObj2Cost(i,ind);
+					z_obj1 += data.getAllocationObjCost(0, i, ind);
+					z_obj2 += data.getAllocationObjCost(1, i, ind);
 				}
 			}
 			
-			if((z_obj1 + alpha * z_obj2) <  (vectorBox[it]->getMinZ1() + alpha * vectorBox[it]->getMaxZ2()))
-                //We found a solution. We split the box in 2. We copy it and modify its bounds
+			if((z_obj1 + alpha * z_obj2) < (vectorBox[it]->getMinZ(0) + alpha * vectorBox[it]->getMaxZ(1)))
+			//We found a solution. We split the box in 2. We copy it and modify its bounds
 			{
 				Box* copyBox = new Box(vectorBox[it]);				
-				vectorBox[it]->setMaxZ1(z_obj1);
-				vectorBox[it]->setMinZ2(z_obj2);
-				copyBox->setMinZ1(z_obj1);
-				copyBox->setMaxZ2(z_obj2);
+				vectorBox[it]->setMaxZ(0, z_obj1);
+				vectorBox[it]->setMinZ(1, z_obj2);
+				copyBox->setMinZ(0, z_obj1);
+				copyBox->setMaxZ(1, z_obj2);
 				vectorBox.push_back(copyBox);
 			}
 			else
@@ -327,7 +325,7 @@ long int runLabelSetting(std::vector<Box*> &vectorBox, Data &data)
 	std::list<Solution> allSolution;
 	std::list<Solution>::iterator it;
 	std::vector<Box*>::iterator itVector;
-	for(itVector = vectorBox.begin(); itVector != vectorBox.end(); itVector++)
+	for(itVector = vectorBox.begin(); itVector != vectorBox.end(); ++itVector)
 	{						
 		LabelSetting *m = new LabelSetting(*(*itVector));
 		m->compute();
@@ -349,16 +347,16 @@ long int runLabelSetting(std::vector<Box*> &vectorBox, Data &data)
 void filterListSolution(std::list<Solution> &lsol)
 {
     std::list<Solution>::iterator iterSuivant = lsol.begin();
-    iterSuivant++;
+    ++iterSuivant;
     std::list<Solution>::iterator iter        = lsol.begin();
     while(iterSuivant != lsol.end())
     {
-        if( (*iter).getObj1() == (*iterSuivant).getObj1() )
+        if( (*iter).getObj(0) == (*iterSuivant).getObj(0) )
         {
             //We have to delete one of these two elements
-            if( (*iter).getObj2() > (*iterSuivant).getObj2() )
+            if( (*iter).getObj(1) > (*iterSuivant).getObj(1) )
             {
-                iterSuivant++;
+                ++iterSuivant;
                 iter = lsol.erase(iter);
             }
             else
@@ -369,10 +367,10 @@ void filterListSolution(std::list<Solution> &lsol)
         }
         else
         {
-            if( (*iter).getObj2() > (*iterSuivant).getObj2() )
+            if( (*iter).getObj(1) > (*iterSuivant).getObj(1) )
             {
-                iter++;
-                iterSuivant ++;
+                ++iter;
+                ++iterSuivant;
             }
             else
             {
@@ -387,28 +385,28 @@ double computeCorrelation(Data &data)
 	double correl(0);
 	double meanX(0), meanY(0), num(0), den1(0), den2(0);
     
-	for(unsigned int i = 0; i < data.getnbCustomer(); i++)
+	for(unsigned int i = 0; i < data.getnbCustomer(); ++i)
 	{
-		for(unsigned int j = 0; j < data.getnbFacility(); j++)
+		for(unsigned int j = 0; j < data.getnbFacility(); ++j)
 		{
-			meanX += data.getAllocationObj1Cost(i,j);
-			meanY += data.getAllocationObj2Cost(i,j);
+			meanX += data.getAllocationObjCost(0, i, j);
+			meanY += data.getAllocationObjCost(1, i, j);
 		}
 	}
 	meanX /= ( data.getnbCustomer() * data.getnbFacility() );
 	meanY /= ( data.getnbCustomer() * data.getnbFacility() );
     
-	for(unsigned int i = 0; i < data.getnbCustomer(); i++)
+	for(unsigned int i = 0; i < data.getnbCustomer(); ++i)
 	{
-		for(unsigned int j = 0; j < data.getnbFacility(); j++)
+		for(unsigned int j = 0; j < data.getnbFacility(); ++j)
 		{
-			num += (data.getAllocationObj1Cost(i,j) - meanX) * (data.getAllocationObj2Cost(i,j) - meanY) ;
-			den1 += pow((data.getAllocationObj1Cost(i,j) - meanX),2);
-			den2 += pow((data.getAllocationObj2Cost(i,j) - meanY),2);
+			num += (data.getAllocationObjCost(0, i, j) - meanX) * (data.getAllocationObjCost(1, i, j) - meanY) ;
+			den1 += pow((data.getAllocationObjCost(0, i, j) - meanX), 2);
+			den2 += pow((data.getAllocationObjCost(1, i, j) - meanY), 2);
 		}
 	}
     
-	correl = num / (sqrt(den1)*sqrt(den2));
+	correl = num / (std::sqrt(den1) * std::sqrt(den2));
 
 	if (Argument::mode_export)
 	{
